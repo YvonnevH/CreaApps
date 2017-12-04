836c896fb2d292b25473732cbe095d9fb9a42aa3
YvonnevH <y.m.v.hamersveld@student.tue.nl>
2017-12-04 12:24:14 +0100

    Initial commit

---
+++ controllers/mobile/HomeController/HomeControllerActions.js
@@ -0,0 +1,17 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_Button_d5697ce5a21d44e796e9a6b68ae565e6: function AS_Button_d5697ce5a21d44e796e9a6b68ae565e6(eventobject) {
+        var self = this;
+        this.view.folderContainer2.isVisible = true;
+    },
+    AS_Camera_i593af700b3e4930903b57ba534987f9: function AS_Camera_i593af700b3e4930903b57ba534987f9(eventobject) {
+        var self = this;
+        this.view.Image0e5627f02a4aa44.rawBytes = this.view.Camera0b5b12a35786b44.rawBytes;
+    },
+    AS_Camera_dde0191e31554331b612247e61714818: function AS_Camera_dde0191e31554331b612247e61714818(eventobject) {
+        var self = this;
+        this.view.Image0e2df4ec8117946.rawBytes = this.view.cameraIcon2.rawBytes;
+    }
+});
